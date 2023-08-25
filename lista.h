f220e7d7fa71ed589c53e0fcf013145790263942
Alejandro Gutiérrez <alejandrogutierrezchaves@gmail.com>
2023-08-25 09:46:21 -0600

    cambios finales

--- lista.h
+++ lista.h
@@ -1,74 +1,31 @@
-// Se usará una lista enlazada simple
-class Nodo {
-public:
-    int valor;
-    Nodo* siguiente;
+// Se usará una lista doble enlazada
 
-    Nodo(int val) : valor(val), siguiente(nullptr) {}
-};
+// Métodos de la lista según lectura
 
-class ListaSimple {
-private:
-    Nodo* cabeza;
+#include <iostream>
+#include <string>
 
-public:
-    ListaSimple() : cabeza(nullptr) {}
+class doublelinkedList {
+public: 
+    doublelinkedList();
 
-    // Método para insertar un elemento en la lista al final de esta
-    void insertarAlFinal(int val) {
-        Nodo* nuevo_nodo = new Nodo(val);
-        if (!cabeza) {
-            cabeza = nuevo_nodo;
-        } else {
-            Nodo* nodo_actual = cabeza;
-            while (nodo_actual->siguiente) {
-                nodo_actual = nodo_actual->siguiente;
-            }
-            nodo_actual->siguiente = nuevo_nodo;
-        }
-    }
+    bool empty(); // ir construyendo la lista
 
-    void eliminar(int val) {
-        if (!cabeza) {
-            return;  // La lista está vacía, no se puede eliminar nada
-        }
+    void getNext(); // obtener el elemento siguiente al nodo actual
 
-        if (cabeza->valor == val) {
-            Nodo* nodo_a_eliminar = cabeza;
-            cabeza = cabeza->siguiente;
-            delete nodo_a_eliminar;
-            return;
-        }
+    void getPrevious(); // obtener el elemento anterior al nodo actual
 
-        Nodo* nodo_anterior = cabeza;
-        while (nodo_anterior->siguiente && nodo_anterior->siguiente->valor != val) {
-            nodo_anterior = nodo_anterior->siguiente;
-        }
+    void addFront(data); // Agregar un elemeto al inicio de la lista 
 
-        if (nodo_anterior->siguiente) {
-            Nodo* nodo_a_eliminar = nodo_anterior->siguiente;
-            nodo_anterior->siguiente = nodo_a_eliminar->siguiente;
-            delete nodo_a_eliminar;
-        }
+    void addBack(data); // Agregar un elemeto al final de la lista
 
-    // Método para imprimir los elementos de la lista
-    void imprimir() {
-        Nodo* nodo_actual = cabeza;
-        while (nodo_actual) {
-            std::cout << nodo_actual->valor << " -> ";
-            nodo_actual = nodo_actual->siguiente;
-        }
-        std::cout << "nullptr" << std::endl;
-    }
+    void remove(data); // Eliminar un elemeto de la lista
+
+    int largoLista (); // Determinar la cantidad de elementos
+
+    void  searchTitular(data); // Buscar titulares en específico
+
+    void searchPosition(int pIndex); // Buscar la posición de un titular en específico
+
+    void mostrarTitular(data); // Mostrar titulares luego de que se buscan
 }
-
-int main() {
-    ListaSimple lista;
-    lista.insertarAlInicio(3);
-    lista.insertarAlInicio(7);
-    lista.insertarAlFinal(12);
-    lista.imprimir();  // Salida: 7 -> 3 -> 12 -> nullptr
-    return 0;
-}
-
-};
